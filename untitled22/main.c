5248d4e5f6d1c87463c83b51d30d00b63a291e4f
canhaktan <143180357+canhaktan@users.noreply.github.com>
2024-01-08 14:01:16 +0300

    Add files via upload

---
+++ untitled22/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* P(x) = x^4 + 6x^2 + 6 */
+/* degree:
+ * coefficient: */
+
+int readPolynomial(double coefficients[], int degrees[])
+{
+    for (int i = 0; i < 20 ; ++i) {
+        
+    }
+
+}
+int main() {
+    int degree, coefficient;
+
+     double coefficients[20];
+     int degrees[20];
+    for (int i = 0; i < 20; ++i) {
+        scanf("%d",&degree);
+        
+    }
+    for (int i = 0; i < 20; ++i) {
+        scanf("%d",&coefficient);
+    }
+    return 0;
+}
